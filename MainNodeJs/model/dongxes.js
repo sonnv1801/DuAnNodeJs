221142a6ce7c8fc43d99acb3956daf411760d739
Your Name <70565643+sonnv1801@users.noreply.github.com>
2021-12-02 18:04:22 +0700

    del

--- MainNodeJs/model/dongxes.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require('mongoose');
-
-var dongxesSchema = new mongoose.Schema({
-    DongXe: String,
-    HangXe: String,
-    SoChoNgoi: String,
-});
-
-module.exports = mongoose.model("DongXe", dongxesSchema)
